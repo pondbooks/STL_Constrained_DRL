5e89bb995735a04161b824241a3b11a2d2822931
pondbooks <68591842+pondbooks@users.noreply.github.com>
2022-05-20 16:27:31 +0900

    Add files via upload

---
+++ Sec_5_2/no_pre_process/DeepRL_code/lagrangian_sac.py
@@ -0,0 +1,285 @@
+import numpy as np
+import torch
+from torch import nn
+from torch.distributions import Normal
+import torch.nn.functional as F
+
+import replay_memory
+import init_state_memory
+import network
+import trainer
+
+class LagrangianSAC(trainer.Algorithm):
+
+    def __init__(self, state_shape, action_shape, device=torch.device('cuda'), seed=0,
+                batch_size=64, gamma=0.99, lr_actor=3e-4, lr_critic=3e-4, lr_entropy=3e-4, lr_kappa=1e-5, threshold=-40.,
+                pretrain_steps=250000, replay_size=10**6, start_steps=10**4, tau=0.01, alpha=1.0, kappa=1.0, reward_scale=1.0, auto_coef=True):
+        super().__init__()
+
+
+        # Initialize the Replay buffer
+        self.buffer = replay_memory.ReplayBuffer(
+            buffer_size=replay_size,
+            state_shape=state_shape,
+            action_shape=action_shape,
+            device=device,
+        )
+
+        # Initialize the Replay Buffer for initial states 
+        self.init_state_buffer = init_state_memory.InitStateBuffer(
+            buffer_size=replay_size,
+            state_shape=state_shape,
+            device=device,
+        )
+
+        # Initialize DNNs
+        self.actor = network.SACActor(
+            state_shape=state_shape,
+            action_shape=action_shape
+        ).to(device)
+        self.reward_critic = network.SACCritic(
+            state_shape=state_shape,
+            action_shape=action_shape
+        ).to(device)
+        self.reward_critic_target = network.SACCritic(
+            state_shape=state_shape,
+            action_shape=action_shape
+        ).to(device).eval()
+        self.STL_critic = network.SACCritic(
+            state_shape=state_shape,
+            action_shape=action_shape
+        ).to(device)
+        self.STL_critic_target = network.SACCritic(
+            state_shape=state_shape,
+            action_shape=action_shape
+        ).to(device).eval()
+
+        # Flag for auto_coef
+        self.auto_coef = auto_coef
+
+        self.alpha = alpha 
+        if self.auto_coef: 
+            # Target Entropy = −dim(A) https://github.com/pranz24/pytorch-soft-actor-critic/blob/master/sac.py
+            self.target_entropy = -torch.prod(torch.Tensor(action_shape).to(device)).item()
+            self.log_alpha = torch.tensor(0.0, requires_grad=True, device=device)
+            self.alpha = self.log_alpha.exp() # 1.0
+
+        # Lagrangian multiplier
+        self.threshold = threshold
+        self.log_kappa = torch.tensor(0.0, requires_grad=True, device=device) 
+        self.kappa = self.log_kappa.exp() # 1.0
+
+        # Initialize target DNNs (reward critic and stl-reward critic)
+        self.reward_critic_target.load_state_dict(self.reward_critic.state_dict())
+        for param in self.reward_critic_target.parameters(): # Cut the grad information
+            param.requires_grad = False
+        self.STL_critic_target.load_state_dict(self.STL_critic.state_dict())
+        for param in self.STL_critic_target.parameters(): # Cut the grad information
+            param.requires_grad = False
+
+        # Define the optimizer (Adam)
+        self.optim_actor = torch.optim.Adam(self.actor.parameters(), lr=lr_actor)
+        self.optim_reward_critic = torch.optim.Adam(self.reward_critic.parameters(), lr=lr_critic)
+        self.optim_STL_critic = torch.optim.Adam(self.STL_critic.parameters(), lr=lr_critic)
+        if self.auto_coef:
+            self.optim_alpha = torch.optim.Adam([self.log_alpha], lr=lr_entropy)
+        self.optim_kappa = torch.optim.Adam([self.log_kappa], lr=lr_kappa) 
+        
+        # other hyper-parameter
+        self.learning_steps = 0 # learning counter
+        self.batch_size = batch_size
+        self.pretrain_steps = pretrain_steps
+        self.device = device
+        self.gamma = gamma
+        self.start_steps = start_steps
+        self.tau = tau
+        self.reward_scale = reward_scale
+
+    def explore(self, state):
+        """ Return a stochastic action a~\pi and its logarithmic probability density \log(\pi(a|s)). """
+        state = torch.tensor(state, dtype=torch.float, device=self.device).unsqueeze_(0)
+        with torch.no_grad():
+            action, log_pi = self.actor.sample(state)
+        return action.cpu().numpy()[0], log_pi.item()
+    
+    def exploit(self, state):
+        """ Return a deterministic action (mean). """
+        state = torch.tensor(state, dtype=torch.float, device=self.device).unsqueeze_(0)
+        with torch.no_grad():
+            action = self.actor(state)
+        return action.cpu().numpy()[0]
+
+    def is_update(self, steps):
+        # The agent does not learn its policy for a certain period (start_steps)．
+        return steps >= max(self.start_steps, self.batch_size)
+    
+    def init_state_stock(self, init_state):
+        # Add state data sampled from the initial state density．
+        self.init_state_buffer.append(init_state)
+
+    def step(self, env, state, t, steps): # function for exploration
+        if t == 0:
+            self.init_state_buffer.append(state)
+
+        t += 1
+
+        # For a certain period (start_steps), the agent determine an action randomly to collect diverse data．
+        if steps <= self.start_steps:
+            action = env.action_space.sample()
+        else:
+            action, _ = self.explore(state)
+        next_state, reward, stl_reward, done, _ = env.step(action)
+
+        if t == env._max_episode_steps:
+            done_masked = False
+        else:
+            done_masked = done
+
+        # Add experience to the replay buffer．
+        self.buffer.append(state, action, reward, stl_reward, done_masked, next_state)
+
+        # Reset env at the end of the episode．
+        if done:
+            t = 0
+            next_state = env.reset()
+
+        return next_state, t
+
+    def update(self, steps): # main algorithm
+        self.learning_steps += 1
+        states, actions, rewards, stl_rewards, dones, next_states = self.buffer.sample(self.batch_size)
+
+        self.update_reward_critic(states, actions, rewards, dones, next_states)
+
+        if steps == self.pretrain_steps:
+            print("===== END PRETRAIN =====")
+        
+        if steps < self.pretrain_steps:
+            self.update_pretrain_STL_critic(states, actions, stl_rewards, dones, next_states)
+            self.update_pretrain_actor(states)
+        else:
+            self.update_finetune_STL_critic(states, actions, stl_rewards, dones, next_states)
+            self.update_finetune_actor(states)
+
+        if self.auto_coef:
+            self.update_entropy_coef(states)
+
+        if steps >= self.pretrain_steps:   
+            init_states = self.init_state_buffer.sample(self.batch_size) 
+            self.update_kappa(init_states)
+
+        self.update_target()    
+    
+    def update_reward_critic(self, states, actions, rewards, dones, next_states):
+        curr_qs1, curr_qs2 = self.reward_critic(states, actions)
+
+        with torch.no_grad():
+            next_actions, log_pis = self.actor.sample(next_states)
+            next_qs1, next_qs2 = self.reward_critic_target(next_states, next_actions)
+            next_qs = torch.min(next_qs1, next_qs2) - self.alpha * log_pis
+        target_qs = rewards * self.reward_scale + (1.0 - dones) * self.gamma * next_qs
+
+        loss_critic1 = (curr_qs1 - target_qs).pow_(2).mean()
+        loss_critic2 = (curr_qs2 - target_qs).pow_(2).mean()
+
+        self.optim_reward_critic.zero_grad()
+        (loss_critic1 + loss_critic2).backward(retain_graph=False)
+        self.optim_reward_critic.step()
+
+    def update_pretrain_STL_critic(self, states, actions, stl_rewards, dones, next_states):
+        curr_stl_qs1, curr_stl_qs2 = self.STL_critic(states, actions)
+
+        with torch.no_grad():
+            next_actions, log_pis = self.actor.sample(next_states)
+            next_stl_qs1, next_stl_qs2 = self.STL_critic_target(next_states, next_actions)
+            next_stl_qs = torch.min(next_stl_qs1, next_stl_qs2) - self.alpha * log_pis # Pretrain
+        target_stl_qs = stl_rewards * self.reward_scale + (1.0 - dones) * self.gamma * next_stl_qs
+
+        loss_stl_critic1 = (curr_stl_qs1 - target_stl_qs).pow_(2).mean()
+        loss_stl_critic2 = (curr_stl_qs2 - target_stl_qs).pow_(2).mean()
+
+        self.optim_STL_critic.zero_grad()
+        (loss_stl_critic1 + loss_stl_critic2).backward(retain_graph=False)
+        self.optim_STL_critic.step()
+
+    def update_finetune_STL_critic(self, states, actions, stl_rewards, dones, next_states):
+        curr_stl_qs1, curr_stl_qs2 = self.STL_critic(states, actions)
+
+        with torch.no_grad():
+            next_actions, _ = self.actor.sample(next_states)
+            next_stl_qs1, next_stl_qs2 = self.STL_critic_target(next_states, next_actions)
+            next_stl_qs = torch.min(next_stl_qs1, next_stl_qs2) # Finetune
+        target_stl_qs = stl_rewards * self.reward_scale + (1.0 - dones) * self.gamma * next_stl_qs
+
+        loss_stl_critic1 = (curr_stl_qs1 - target_stl_qs).pow_(2).mean()
+        loss_stl_critic2 = (curr_stl_qs2 - target_stl_qs).pow_(2).mean()
+
+        self.optim_STL_critic.zero_grad()
+        (loss_stl_critic1 + loss_stl_critic2).backward(retain_graph=False)
+        self.optim_STL_critic.step()
+
+    def update_pretrain_actor(self, states):
+        actions, log_pis = self.actor.sample(states)
+        STL_qs1, STL_qs2 = self.STL_critic(states, actions)
+        loss_actor = (self.alpha * log_pis - torch.min(STL_qs1, STL_qs2)).mean() # Pretrain
+
+        self.optim_actor.zero_grad()
+        loss_actor.backward(retain_graph=False)
+        self.optim_actor.step()
+
+    def update_finetune_actor(self, states):
+        actions, log_pis = self.actor.sample(states)
+        reward_qs1, reward_qs2 = self.reward_critic(states, actions)
+        STL_qs1, STL_qs2 = self.STL_critic(states, actions)
+        loss_actor = (self.alpha * log_pis - torch.min(reward_qs1, reward_qs2) - self.kappa * (torch.min(STL_qs1, STL_qs2))).mean() # Finetune
+
+        self.optim_actor.zero_grad()
+        loss_actor.backward(retain_graph=False)
+        self.optim_actor.step()
+
+    def update_entropy_coef(self, states):
+        _, log_pis = self.actor.sample(states)
+        alpha_loss = -(self.log_alpha.exp() * (log_pis + self.target_entropy).detach()).mean()
+        #alpha_loss = -(self.log_alpha * (log_pis + self.target_entropy).detach()).mean()
+
+        self.optim_alpha.zero_grad()
+        alpha_loss.backward()
+        self.optim_alpha.step()
+
+        self.alpha = self.log_alpha.exp()
+
+    def update_kappa(self, states):
+        actions, _ = self.actor.sample(states)
+        STL_qs1, STL_qs2 = self.STL_critic(states, actions)
+        STL_qs = torch.min(STL_qs1, STL_qs2)
+
+        kappa_loss = self.log_kappa.exp() * ((STL_qs - self.threshold).detach()).mean()
+        #kappa_loss = self.log_kappa * ((STL_qs - self.threshold).detach()).mean()
+
+        self.optim_kappa.zero_grad()
+        kappa_loss.backward()
+        self.optim_kappa.step()
+
+        self.kappa = self.log_kappa.exp()
+
+    def update_target(self):
+        for t, s in zip(self.reward_critic_target.parameters(), self.reward_critic.parameters()):
+            t.data.mul_(1.0 - self.tau)
+            t.data.add_(self.tau * s.data)
+        for t, s in zip(self.STL_critic_target.parameters(), self.STL_critic.parameters()):
+            t.data.mul_(1.0 - self.tau)
+            t.data.add_(self.tau * s.data)
+    
+    def backup_model(self, steps):
+        #model = self.actor.to('cpu')
+        torch.save(self.actor.state_dict(), 'SAC_STL_Actor_' + str(steps) + '.pth')
+        torch.save(self.reward_critic.state_dict(), 'SAC_Reward_Critic_' + str(steps) + '.pth')
+        torch.save(self.STL_critic.state_dict(), 'SAC_STL_Critic_' + str(steps) + '.pth')
+
+        
+
+
+
+
+
+
---
+++ Sec_5_2/no_pre_process/gym_pathplan/envs/stl_pathplan.py
@@ -0,0 +1,382 @@
+import numpy as np
+import math
+
+import gym
+from gym import error, spaces, utils
+from gym.utils import seeding
+
+import sys
+import os
+
+# If you want to output gif animation, import the rendering from classical_control 
+# from gym.envs.classic_control import rendering  
+
+class STL_Problem_GF(gym.Env):
+
+    metadata = {'render.modes': ['human', 'rgb_array']}
+
+    def __init__(self):
+        # The area of render is [(0.0,0.0),(0.0,max_y),(max_x,0.0),(max_x,max_y)]
+        self._max_x_of_window = 5.0
+        self._max_y_of_window = 5.0
+
+        self.dt = 0.1 # The sampling period of the dynamical system.
+        self._max_episode_steps = 1000
+
+        # The time bound of subSTL
+        hrz_phi_1 = 99.
+        hrz_phi_2 = 99.
+        hrz_phi = max(hrz_phi_1,hrz_phi_2) # 99
+        self.phi_1_timebound = [0.0, hrz_phi_1]
+        self.phi_2_timebound = [0.0, hrz_phi_2]
+        self.tau = int(hrz_phi + 1) # hrz(phi) + 1 = 100
+        self.tau_1 = hrz_phi_1 # 99.
+        self.tau_2 = hrz_phi_2 # 99.
+
+        # log-sum-exp app parameter
+        self.beta = 100.
+
+        # robot param
+        #self.robot_radius = 0.2 #[m]
+
+        # action param
+        self.max_velocity = 1.0   # [m/s]
+        self.min_velocity = -1.0  # [m/s]
+        self.min_angular_velocity = math.radians(-90)  # [rad/s]
+        self.max_angular_velocity = math.radians(90) # [rad/s]
+
+        self.num_steps = 0
+
+        # [car_x, car_y, car_yaw, flag_1, flag_2]
+        self.high = np.array([np.inf, np.inf, np.pi], dtype=np.float32)
+        self.low = np.array([-np.inf, -np.inf, -np.pi], dtype=np.float32)
+        self.car_dim = 3
+        #self.low_extended_space = np.array([-np.inf, -np.inf, -np.pi, -0.5 , -0.5], dtype=np.float32)# return obs dim (preprocessed)
+        #self.high_extended_space =  np.array([np.inf, np.inf, np.pi, 0.5 , 0.5], dtype=np.float32)
+        self.low_extended_space = -100*np.ones(self.tau*self.car_dim)
+        self.high_extended_space =  100*np.ones(self.tau*self.car_dim)
+
+        # set action_space (velocity[m/s], omega[rad/s])
+        self.action_low  = np.array([self.min_velocity, self.min_angular_velocity]) 
+        self.action_high = np.array([self.max_velocity, self.max_angular_velocity]) 
+
+        self.action_space = spaces.Box(low=self.action_low, high=self.action_high, shape =(2,), dtype=np.float32)
+        self.observation_space = spaces.Box(\
+            low=self.low,\
+            high=self.high,\
+            dtype=np.float32 
+        )
+        self.extended_state_space = spaces.Box(\
+            low=self.low_extended_space,\
+            high=self.high_extended_space,\
+            dtype=np.float32
+        )
+
+        self.seed()
+        self.viewer = None
+        self.vis_lidar = True
+        self.num_steps = 0 # step counter
+
+        # ##################
+        # initial state region
+        self.init_low_x = 0.5
+        self.init_low_y = 0.5
+        self.init_high_x = 2.5
+        self.init_high_y = 2.5
+
+        # phi_1
+        self.stl_1_low_x = 3.5
+        self.stl_1_low_y = 3.5
+        self.stl_1_high_x = 4.5
+        self.stl_1_high_y = 4.5
+
+        #phi_2
+        self.stl_2_low_x = 3.5
+        self.stl_2_low_y = 1.5
+        self.stl_2_high_x = 4.5
+        self.stl_2_high_y = 2.5
+
+        # better region
+        self.better_region_low_x = 0.5
+        self.better_region_low_y = 0.5
+        self.better_region_high_x = 4.5
+        self.better_region_high_y = 4.5
+        # ##################
+
+    def reset(self): 
+        # initial state [x(m), y(m), yaw(rad)]
+        init_x = self.np_random.uniform(low=self.init_low_x, high=self.init_high_x)
+        init_y = self.np_random.uniform(low=self.init_low_y, high=self.init_high_y)
+        init_rad = self.np_random.uniform(low=-np.pi/2, high=np.pi/2)
+
+        self.past_tau_trajectory = [] # past trajectory memory [s_{t-tau+1},s_{t-tau+2},...,s_{t}]
+        for i in range(self.tau): # The length is tau
+            current_state = np.array([init_x, init_y, init_rad])
+            self.past_tau_trajectory.append(current_state)
+
+        self.num_steps = 0 # counter of exploration steps
+
+        self.state = np.array([init_x, init_y, init_rad]) # the current state of the car
+
+        #self.observation = self.preprocess(self.past_tau_trajectory) 
+        self.observation = self.observe(self.past_tau_trajectory) # preprocessing
+        
+        self.done = False
+        #self.success_val = 0.0 # GF
+
+        return self.observation
+
+    def step(self, action): 
+
+        stl_reward = self.STLreward(self.past_tau_trajectory) # stl reward
+        reward = self.reward(self.state, action) # low-level reward
+
+        noise_w0 = 0.1*np.random.normal(0,1) 
+        noise_w1 = 0.1*np.random.normal(0,1)
+        noise_w2 = 0.1*np.random.normal(0,1)
+
+        # next step ================================================
+        self.state[0] += (action[0] * math.cos(self.state[2]) + noise_w0) * self.dt
+        self.state[1] += (action[0] * math.sin(self.state[2]) + noise_w1) * self.dt
+        self.state[2] += (action[1] + noise_w2) * self.dt 
+
+        if self.state[2] < -np.pi:
+            #self.state[2] += math.pi * 2.0
+            self.state[2] += np.pi * 2.0
+        elif math.pi < self.state[2]:
+            #self.state[2] -= math.pi * 2.0
+            self.state[2] -= np.pi * 2.0
+        #======================================================================
+
+        self.past_tau_trajectory = self.past_tau_trajectory[1:] # update the extended state
+        self.past_tau_trajectory.append(self.state.copy())
+
+        #self.observation = self.preprocess(self.past_tau_trajectory) # preprocessing
+        self.observation = self.observe(self.past_tau_trajectory) # no preprocessing
+        
+        self.num_steps += 1
+
+        if self.num_steps == self._max_episode_steps: # 1000 steps
+            return_done = True
+            self.reset() 
+        else:
+            return_done = False
+
+        return self.observation, reward, stl_reward, return_done, {}
+
+    #def preprocess(self, tau_state): # return pre-processed tau_mdp state
+    #    tau_num = len(tau_state)
+    #    assert tau_num == self.tau, "dim of tau-state is wrong."
+    #    obs = np.zeros(self.car_dim + 2) # dim(obs) = 5
+    #    obs[0] = tau_state[tau_num-1][0] - (self._max_x_of_window/2)
+    #    obs[1] = tau_state[tau_num-1][1] - (self._max_y_of_window/2)
+    #    obs[2] = tau_state[tau_num-1][2]
+
+    #    f1 = 0.0
+    #    f2 = 0.0 
+    #    for i in range(tau_num):
+    #        if self.subSTL_1_robustness(tau_state[i]) >= 0:
+    #            f1 = 1.0
+    #        else:
+    #            f1 = max(f1 - 1/(float(self.tau_1 + 1)), 0.0)
+    #        if self.subSTL_2_robustness(tau_state[i]) >= 0:
+    #            f2 = 1.0
+    #        else:
+    #            f2 = max(f2 - 1/(float(self.tau_2 + 1)), 0.0)
+    #    obs[3] = f1 - 0.5
+    #    obs[4] = f2 - 0.5
+    #    return obs
+
+    # nopreprocessing
+    def observe(self, tau_state): 
+        tau_num = len(tau_state)
+        assert tau_num == self.tau, "dim of tau-state is wrong."
+
+        # Define tau extended state
+        obs = np.zeros(tau_num*self.car_dim) # 100*3 
+        for i in range(tau_num):
+            obs[self.car_dim*i] = tau_state[i][0] - (self._max_x_of_window/2)
+            obs[self.car_dim*i + 1] = tau_state[i][1] - (self._max_y_of_window/2)
+            obs[self.car_dim*i + 2] = tau_state[i][2]
+        return obs
+
+    def STLreward(self, tau_state): 
+        tau_num = len(tau_state)
+        phi_1_rob = -500. # Tentative value
+        phi_2_rob = -500.
+
+        for i in range(tau_num):
+            temp_1_rob = self.subSTL_1_robustness(tau_state[i])
+            temp_2_rob = self.subSTL_2_robustness(tau_state[i])
+            phi_1_rob = max(phi_1_rob, temp_1_rob) # F \varphi
+            phi_2_rob = max(phi_2_rob, temp_2_rob) # F \varphi
+
+        return_val = min(phi_1_rob, phi_2_rob) # land
+
+        # indicator I(x)
+        if return_val >= 0:
+            return_val = 1.0
+        else:
+            return_val = 0.0
+
+        stl_reward = -np.exp(-self.beta*return_val)
+
+        return stl_reward
+
+    def reward(self, state, action):
+        return_val = - action[0]**2
+        return_val += - action[1]**2
+        return_val += self.better_region(state)
+        return return_val
+
+    def evaluate_stl_formula(self): # called by trainer to evaluate satisfying the specification
+        if self.num_steps >= self.tau-1: 
+            tau_num = len(self.past_tau_trajectory)
+            phi_1_rob = -500. # Tentative value
+            phi_2_rob = -500.
+
+            for i in range(tau_num):
+                temp_1_rob = self.subSTL_1_robustness(self.past_tau_trajectory[i])
+                temp_2_rob = self.subSTL_2_robustness(self.past_tau_trajectory[i])
+                phi_1_rob = max(phi_1_rob, temp_1_rob)
+                phi_2_rob = max(phi_2_rob, temp_2_rob)
+
+            returns = min(phi_1_rob, phi_2_rob) # land
+
+            if returns >= 0:
+                returns = 1.0
+            else:
+                returns = 0.0
+
+        else: # t < tau-1 
+            returns = 1.0 
+
+        return returns
+
+    def subSTL_1_robustness(self, state):
+        psi1 = state[0] - self.stl_1_low_x # 3.5 < x < 4.5
+        psi2 = self.stl_1_high_x - state[0]
+        psi3 = state[1] - self.stl_1_low_y # 3.5 < y < 4.5
+        psi4 = self.stl_1_high_y - state[1]
+        robustness = min(psi1, psi2)
+        robustness = min(robustness, psi3)
+        robustness = min(robustness, psi4)
+        return robustness
+    
+    def subSTL_2_robustness(self, state):
+        psi1 = state[0] - self.stl_2_low_x # 3.5 < x < 4.5
+        psi2 = self.stl_2_high_x - state[0]
+        psi3 = state[1] - self.stl_2_low_y # 1.5 < y < 2.5
+        psi4 = self.stl_2_high_y - state[1]
+        robustness = min(psi1, psi2)
+        robustness = min(robustness, psi3)
+        robustness = min(robustness, psi4)
+        return robustness
+
+    def better_region(self,state):
+        psi1 = state[0] - self.better_region_low_x  # 0.5 < x < 4.5
+        psi2 = self.better_region_high_x - state[0]
+        psi3 = state[1] - self.better_region_low_y # 0.5 < y < 4.5
+        psi4 = self.better_region_high_y - state[1]
+        robustness = min(psi1, psi2)
+        robustness = min(robustness, psi3)
+        robustness = min(robustness, psi4)
+        robustness = min(robustness, 0.0) # nonpositive
+        return robustness
+
+    ''' def render(self, mode='human', close=False): # 環境を可視化する
+        screen_width  = 300
+        screen_height = 300
+
+        rate_x = screen_width / self._max_x_of_window
+        rate_y = screen_height / self._max_y_of_window 
+
+        rate_init_l = self.init_low_x / self._max_x_of_window
+        rate_init_r = self.init_high_x / self._max_x_of_window
+        rate_init_t = self.init_high_y / self._max_y_of_window
+        rate_init_b = self.init_low_y / self._max_y_of_window
+        rate_stl_1_l = self.stl_1_low_x / self._max_x_of_window
+        rate_stl_1_r = self.stl_1_high_x / self._max_x_of_window
+        rate_stl_1_t = self.stl_1_high_y / self._max_y_of_window
+        rate_stl_1_b = self.stl_1_low_y / self._max_y_of_window
+        rate_stl_2_l = self.stl_2_low_x / self._max_x_of_window
+        rate_stl_2_r = self.stl_2_high_x / self._max_x_of_window
+        rate_stl_2_t = self.stl_2_high_y / self._max_y_of_window
+        rate_stl_2_b = self.stl_2_low_y / self._max_y_of_window
+        rate_better_region_l = self.better_region_low_x / self._max_x_of_window
+        rate_better_region_r = self.better_region_high_x / self._max_x_of_window
+        rate_better_region_t = self.better_region_high_y / self._max_y_of_window
+        rate_better_region_b = self.better_region_low_y / self._max_y_of_window
+
+        if self.viewer is None:
+            self.viewer = rendering.Viewer(screen_width, screen_height)
+
+
+            # base
+            b1_l, b1_r, b1_t, b1_b = rate_better_region_l*screen_width, rate_better_region_r*screen_width, rate_better_region_t*screen_height, rate_better_region_b*screen_height  
+            self.b1 = [(b1_l,b1_b), (b1_l,b1_t), (b1_r,b1_t), (b1_r,b1_b)]
+            b1 = rendering.make_polygon(self.b1)
+            self.b1trans = rendering.Transform()
+            b1.add_attr(self.b1trans)
+            b1.set_color(0.8, 0.8, 0.8)
+            self.viewer.add_geom(b1)
+
+            # start
+            start_l, start_r, start_t, start_b = rate_init_l*screen_width, rate_init_r*screen_width, rate_init_t*screen_height, rate_init_b*screen_height  
+            self.start_area = [(start_l,start_b), (start_l,start_t), (start_r,start_t), (start_r,start_b)]
+            start = rendering.make_polygon(self.start_area)
+            self.start_area_trans = rendering.Transform()
+            start.add_attr(self.start_area_trans)
+            start.set_color(0.8, 0.5, 0.5)
+            self.viewer.add_geom(start)
+
+            # goal_1
+            g1_l, g1_r, g1_t, g1_b = rate_stl_1_l*screen_width, rate_stl_1_r*screen_width, rate_stl_1_t*screen_height, rate_stl_1_b*screen_height 
+            self.v1 = [(g1_l,g1_b), (g1_l,g1_t), (g1_r,g1_t), (g1_r,g1_b)]
+            g1 = rendering.make_polygon(self.v1)
+            self.g1trans = rendering.Transform()
+            g1.add_attr(self.g1trans)
+            g1.set_color(0.5, 0.5, 0.8)
+            self.viewer.add_geom(g1)
+
+            # goal_2
+            g2_l, g2_r, g2_t, g2_b = rate_stl_2_l*screen_width, rate_stl_2_r*screen_width, rate_stl_2_t*screen_height, rate_stl_2_b*screen_height  
+            self.v2 = [(g2_l,g2_b), (g2_l,g2_t), (g2_r,g2_t), (g2_r,g2_b)]
+            g2 = rendering.make_polygon(self.v2)
+            self.g2trans = rendering.Transform()
+            g2.add_attr(self.g2trans)
+            g2.set_color(0.5, 0.5, 0.8)
+            self.viewer.add_geom(g2)
+
+            head_x = (self.robot_radius) * rate_x
+            head_y = 0.0 * rate_y
+            tail_left_x = (self.robot_radius*np.cos((5/6)+np.pi)) * rate_x
+            tail_left_y = (self.robot_radius*np.sin((5/6)+np.pi)) * rate_y
+            tail_right_x = (self.robot_radius*np.cos(-(5/6)+np.pi)) * rate_x
+            tail_right_y = (self.robot_radius*np.sin(-(5/6)+np.pi)) * rate_y
+            self.car_v = [(head_x,head_y), (tail_left_x,tail_left_y), (tail_right_x,tail_right_y)]
+            car = rendering.FilledPolygon(self.car_v)
+            self.cartrans = rendering.Transform()
+            car.add_attr(self.cartrans)
+            car.set_color(1.0, 0.0, 0.0)
+            self.viewer.add_geom(car)
+
+        car_x = self.state[0] * rate_x
+        car_y = self.state[1] * rate_y
+   
+
+        self.cartrans.set_translation(car_x, car_y)
+        self.cartrans.set_rotation(self.state[2])
+
+        return self.viewer.render(return_rgb_array = mode=='rgb_array') '''
+
+    def close(self):
+        if self.viewer:
+            self.viewer.close()
+            self.viewer = None
+
+    def seed(self, seed=None):
+        self.np_random, seed = seeding.np_random(seed)
+        return [seed]
+
+
